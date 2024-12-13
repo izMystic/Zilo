911639b9d0bed50ae472dbf2a536ee99431fe296
mystic <izmystic@proton.me>
2024-12-13 02:21:49 -0500

    chore: formatting

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import { CommandKit } from "commandkit";
-import path from "path";
+import path from "node:path";
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 new CommandKit({
   client,
@@ -10,6 +16,7 @@ new CommandKit({
   eventsPath: path.join(__dirname, "events"),
   devGuildIds: ["1157101410710736946"],
   devUserIds: ["680957940647329829", "739219467455823921"],
+  devRoleIds: ["1157101410710736950"],
   skipBuiltInValidations: true,
   bulkRegister: true,
 });
