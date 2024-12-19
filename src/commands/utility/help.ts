4c9b5bbbabdaf9a65e53f98c0d35f495c2ac7b40
mystic <izmystic@proton.me>
2024-12-18 23:34:49 -0500

    feat(commands/utility): help category selector

--- src/commands/utility/help.ts
+++ src/commands/utility/help.ts
@@ -9,25 +9,27 @@ import {
   ButtonBuilder,
   ButtonStyle,
   ComponentType,
+  StringSelectMenuBuilder,
 } from "discord.js";
 import logger from "@src/utils/logger";
+
+export const options: CommandOptions = {
+  devOnly: true,
+};
 
 export const data: CommandData = {
   name: "help",
   description: "Displays a list of available commands.",
 };
 
-export const options: CommandOptions = {
-  devOnly: true,
-};
-
-export async function run({ interaction, client, handler }: SlashCommandProps) {
+export async function run({ interaction, handler }: SlashCommandProps) {
   const commands: { [category: string]: any[] } = {};
 
   if (!handler || !handler.commands) {
     logger.error("Command handler or commands not available.");
     return interaction.reply({
       content: "Could not retrieve command information.",
+      ephemeral: true,
     });
   }
 
@@ -44,89 +46,157 @@ export async function run({ interaction, client, handler }: SlashCommandProps) {
   });
 
   const categories = Object.keys(commands);
-
   if (categories.length === 0) {
-    return interaction.reply({ content: "No commands found." });
+    return interaction.reply({
+      content: "No commands found.",
+      ephemeral: true,
+    });
   }
 
+  let currentCategory: string | null = null;
   let currentPage = 0;
 
-  const generateEmbed = (page: number) => {
-    const category = categories[page];
-    const embed = new EmbedBuilder()
-      .setTitle(
-        `Help - ${category.trim()[0].toUpperCase() + category.trim().substring(1)}`,
+  const generateGeneralInfoEmbed = () => {
+    return new EmbedBuilder()
+      .setTitle("Help - General Information")
+      .addFields(
+        {
+          name: "About Me",
+          value: `Hello I am ${interaction.guild?.members?.me?.displayName}! \n An all in one Discord Bot with auto-moderation, administration, music, and more.`,
+        },
+        {
+          name: "Invite Me",
+          value:
+            "[Here](https://discord.com/oauth2/authorize?client_id=1222574387203670111&permissions=397602323830&scope=bot%20applications.commands)",
+          inline: true,
+        },
+        {
+          name: "Support Server",
+          value: "https://discord.gg/bZBQQa63D6",
+          inline: true,
+        }
       )
-      .setDescription(`List of commands in the ${category} category.`)
+      .setColor("Blue")
+      .setFooter({
+        text: `Total Categories: ${categories.length}`,
+      });
+  };
+
+  const generateCategoryEmbed = (category: string, page: number) => {
+    const embed = new EmbedBuilder()
+      .setTitle(`${category[0].toUpperCase() + category.substring(1)} Commands`)
       .setColor("Blue");
 
     const validCommands = commands[category].filter(
       (command) =>
-        command &&
-        command.data &&
-        command.data.name &&
-        command.data.description,
+        command && command.data && command.data.name && command.data.description
     );
 
-    if (validCommands.length === 0) {
+    const paginatedCommands = validCommands.slice(page * 5, page * 5 + 5);
+
+    if (paginatedCommands.length === 0) {
       embed.addFields({
         name: "No commands in this Category",
         value: "There are no commands in this category.",
       });
-      return embed;
+    } else {
+      paginatedCommands.forEach((command) => {
+        let usage = `/${command.data.name}`;
+        if (command.options && command.options.options) {
+          usage += command.options.options
+            .map((option: CommandOptions) => ` [${option.name}]`)
+            .join("");
+        }
+        embed.addFields({
+          name: `\`${usage}\``,
+          value: command.data.description,
+        });
+      });
     }
 
-    validCommands.forEach((command) => {
-      let usage = `/${command.data.name}`;
-      if (command.options && command.options.options) {
-        usage += command.options.options
-          .map((option: CommandOptions) => ` [${option.name}]`)
-          .join("");
-      }
-      embed.addFields({
-        name: `\`${usage}\``,
-        value: command.data.description,
-      });
+    embed.setFooter({
+      text: `Page ${page + 1} of ${Math.ceil(validCommands.length / 5)}`,
     });
-
-    embed.setFooter({ text: `Page ${page + 1} of ${categories.length}` });
     return embed;
   };
 
-  const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
-    new ButtonBuilder()
-      .setCustomId("prev")
-      .setLabel("Previous")
-      .setStyle(ButtonStyle.Primary)
-      .setDisabled(true),
-    new ButtonBuilder()
-      .setCustomId("next")
-      .setLabel("Next")
-      .setStyle(ButtonStyle.Primary)
-      .setDisabled(categories.length <= 1),
-  );
+  const generateComponents = () => {
+    const row = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+      new StringSelectMenuBuilder()
+        .setCustomId("category_select")
+        .setPlaceholder("Select a category")
+        .addOptions(
+          categories.map((category) => ({
+            label: category[0].toUpperCase() + category.substring(1),
+            value: category,
+          }))
+        )
+    );
+
+    const buttons = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      new ButtonBuilder()
+        .setCustomId("prev")
+        .setLabel("⬅️")
+        .setStyle(ButtonStyle.Primary)
+        .setDisabled(true),
+      new ButtonBuilder()
+        .setCustomId("next")
+        .setLabel("➡️")
+        .setStyle(ButtonStyle.Primary)
+        .setDisabled(true)
+    );
+
+    return { dropdown: row, buttons };
+  };
+
+  const { dropdown, buttons } = generateComponents();
 
   const message = await interaction.reply({
-    embeds: [generateEmbed(currentPage)],
-    components: [row],
+    embeds: [generateGeneralInfoEmbed()],
+    components: [dropdown],
   });
 
   const collector = message.createMessageComponentCollector({
-    componentType: ComponentType.Button,
+    componentType: ComponentType.StringSelect,
     time: 60000,
   });
 
   collector.on("collect", async (i) => {
-    if (i.customId === "prev") {
-      currentPage--;
-    } else if (i.customId === "next") {
-      currentPage++;
-    }
+    const selectedCategory = i.values[0];
+    currentCategory = selectedCategory;
+    currentPage = 0;
 
-    row.components[0].setDisabled(currentPage === 0);
-    row.components[1].setDisabled(currentPage === categories.length - 1);
+    const validCommands = commands[selectedCategory] || [];
+    buttons.components[0].setDisabled(currentPage === 0);
+    buttons.components[1].setDisabled(validCommands.length <= 5);
 
-    await i.update({ embeds: [generateEmbed(currentPage)], components: [row] });
+    await i.update({
+      embeds: [generateCategoryEmbed(selectedCategory, currentPage)],
+      components: [dropdown, buttons],
+    });
+  });
+
+  const buttonCollector = message.createMessageComponentCollector({
+    componentType: ComponentType.Button,
+    time: 60000,
+  });
+
+  buttonCollector.on("collect", async (i) => {
+    if (!currentCategory) return;
+
+    const validCommands = commands[currentCategory] || [];
+    if (i.customId === "prev") currentPage--;
+    else if (i.customId === "next") currentPage++;
+
+    buttons.components[0].setDisabled(currentPage === 0);
+    buttons.components[1].setDisabled(
+      currentPage === Math.ceil(validCommands.length / 5) - 1
+    );
+
+    await i.update({
+      embeds: [generateCategoryEmbed(currentCategory, currentPage)],
+      components: [dropdown, buttons],
+    });
   });
 
   collector.on("end", async () => {
