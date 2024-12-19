424d9f96d59ae1ba0949ec6282f370ab6962f162
mystic <izmystic@proton.me>
2024-12-18 23:35:31 -0500

    chore(events/ready): rename console-log to index

--- src/events/ready/index.ts
+++ src/events/ready/index.ts
@@ -1,11 +1,12 @@
 import type { Client } from "discord.js";
 import type { CommandKit } from "commandkit";
 import logger from "@src/utils/logger";
+import { lavaclient } from "@src/handlers/lavaclient";
 
 export default async function (
   c: Client<true>,
   client: Client<true>,
-  handler: CommandKit,
+  handler: CommandKit
 ) {
   logger.info(`Logged in as ${client.user.tag}! (${client.user.id})`);
 }
