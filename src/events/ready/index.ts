59a9e59992b2c3717ea9e4347045cd3b0d85ad4b
mystic <izmystic@proton.me>
2024-12-18 23:39:10 -0500

    chore(events/ready): remove unused imports

--- src/events/ready/index.ts
+++ src/events/ready/index.ts
@@ -1,12 +1,6 @@
 import type { Client } from "discord.js";
-import type { CommandKit } from "commandkit";
 import logger from "@src/utils/logger";
-import { lavaclient } from "@src/handlers/lavaclient";
 
-export default async function (
-  c: Client<true>,
-  client: Client<true>,
-  handler: CommandKit
-) {
+export default async function (client: Client<true>) {
   logger.info(`Logged in as ${client.user.tag}! (${client.user.id})`);
 }
